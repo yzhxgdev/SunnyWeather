c288ccb9397f1fde048d4d2a48f5f7ff050828ac
yzh <2497479825@qq.com>
2021-01-04 16:29:53 +0800

    second

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -8,7 +8,7 @@ class SunnyWeatherApplication : Application() {
 
     companion object {
 
-        const val TOKEN = "" // 填入你申请到的令牌值
+        const val TOKEN = "4SAuXdz2OLsqA88T" // 填入你申请到的令牌值
 
         @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
